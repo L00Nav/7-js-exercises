d8719ec7f84f9540393b5f1d3612e210b9b87aed
TayakiNoTenshi <luixlt@yahoo.com>
2022-04-06 12:31:03 +0300

    apr06 while

---
+++ js/apr06.js
@@ -0,0 +1,76 @@
+function getRandomInt(max)
+{
+    return Math.floor(Math.random() * max);
+}
+
+//1
+function under100()
+{
+    let sum = 0;
+    while (sum < 100)
+    {
+        sum += getRandomInt(10);
+        console.log(sum);
+    }
+}
+under100();
+console.log('');
+
+
+//2
+function div7()
+{
+    let sum = getRandomInt(10);
+    while (sum % 7)
+    {
+        sum += getRandomInt(10);
+        console.log(sum);
+    }
+}
+div7();
+console.log('');
+
+//3
+function book(pages)
+{
+    let pagesRead = 0;
+    let days = 0;
+    while (pagesRead < pages)
+    {
+        days++;
+        pagesRead += days;
+    }
+    console.log("Days: ", days);
+    console.log("Avg. pages read /day: ", (pages / days).toFixed(2));
+}
+book(8);
+book(17);
+console.log('');
+
+//4
+function randTill5 ()
+{
+    let n = 0;
+    while (n != 5)
+    {
+        n = getRandomInt(10);
+        console.log(n);
+    }
+}
+randTill5();
+console.log('');
+
+//5
+function randTill5or7 ()
+{
+    let n = 0;
+    let cycles = 0;
+    while (n != 5 && n != 7)
+    {
+        n = getRandomInt(10);
+        console.log(n);
+        cycles++;
+    }
+    console.log("Total cycles: ", cycles);
+}
+randTill5or7();
